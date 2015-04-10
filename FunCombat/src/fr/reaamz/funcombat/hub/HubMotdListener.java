2b67a1b9fb4037eaba48b8d4770a383377b7848b
Loucool111 <Loucool111@gmail.com>
2015-04-10 22:50:23 +0200

    Ajout du motd

--- FunCombat/src/fr/reaamz/funcombat/hub/HubMotdListener.java
+++ FunCombat/src/fr/reaamz/funcombat/hub/HubMotdListener.java
@@ -1,14 +1,17 @@
 package fr.reaamz.funcombat.hub;
 
+import java.util.Date;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
 public class HubMotdListener implements Listener
 {
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onServerPingEvent(ServerListPingEvent event)
 	{
-		event.setMotd("§eBienvenue sur FunCombat !\n§41.8 only !");
+		event.setMotd("§eBienvenue sur FunCombat !\n§41.8 only ! " + new Date().toLocaleString());
 	}
 }
